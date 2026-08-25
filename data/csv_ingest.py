49f3da789763d974eea935b995c9e77c47fa3e7c
kleinbuild <kleinbuild@gmail.com>
2026-08-25 14:51:16 -0700

    added column dicts to CSV ingest and fixed variable names. Added try/except. Testing soon, ingest still WIP.

--- data/csv_ingest.py
+++ data/csv_ingest.py
@@ -3,9 +3,112 @@
 import csv
 from csv import DictReader
 
+schools_header_map = {
+    "CDS Code": "cds_code", 
+    "Fed ID": "fed_id",
+    "District Code": "district_code",
+    "School Code": "school_code",
+    "Region": "region",
+    "County Name": "county_name",
+    "District Name": "district_name",
+    "School Name": "school_name",
+    "School Type": "school_type",
+    "Open Date": "open_date",
+    "School Level": "school_level",
+    "Grade Low": "grade_low",
+    "Grade High": "grade_high",
+    "Charter": "charter",
+    "Charter Num": "charter_num",
+    "Street": "street",
+    "City": "city",
+    "Zip": "zip",
+    "State": "state",
+    "Locale": "locale",
+    "School Website": "school_website",
+    "Latitude": "latitude",
+    "Longitude": "longitude"
+}
+
+schools_snapshots_header_map = {
+    "Academic Year": "academic_year",        
+    "CDS Code": "cds_code",             
+    "Charter Funding Type": "charter_fund_type",    
+    "Virtual": "virtual",              
+    "Magnet": "magnet",               
+    "Title I": "title_i",              
+    "DASS": "dass",                 
+    "Assistance Status ESSA": "essa",                 
+    "Enroll Total": "enroll_total",            
+    "African American": "african_amer",            
+    "African American (%)": "african_amer_pct",     
+    "American Indian": "amer_indian",             
+    "American Indian (%)": "amer_indian_pct",      
+    "Asian": "asian",                   
+    "Asian(%)": "asian_pct",            
+    "Filipino": "filipino",                
+    "Filipino (%)": "filipino_pct",                     
+    "Hispanic": "hispanic",                
+    "Hispanic (%)": "hispanic_pct",         
+    "Pacific Islander": "pac_islander",            
+    "Pacific Islander (%)": "pac_islander_pct",     
+    "White": "white",                   
+    "White (%)": "white_pct",            
+    "Two or More Races": "two_or_more_races",       
+    "Two or More Races (%)": "two_or_more_races_pct",
+    "Not Reported": "not_reported",         
+    "Not Reported (%)": "not_reported_pct",     
+    "English Learner": "english_learner",         
+    "English Learner (%)": "english_learner_pct",  
+    "Foster": "foster",                  
+    "Foster (%)": "foster_pct",           
+    "Homeless": "homeless",                
+    "Homeless (%)": "homeless_pct",          
+    "Migrant": "migrant",                 
+    "Migrant (%)": "migrant_pct",          
+    "Socioeconomically Disadvantaged": "soc_disadvantaged",       
+    "Socioeconomically Disadvantaged (%)": "soc_disadvantaged_pct",
+    "Students with Disabilities": "students_with_dis",       
+    "Students with Disabilities (%)": "students_with_dis_pct",
+    "Free/Reduced Meal Eligible": "free_reduced_meal",       
+    "Free/Reduced Meal Eligible (%)": "free_reduced_meal_pct",
+    "Grade TK": "grade_tk",                
+    "Grade KG": "grade_kg",                
+    "Grade 1": "grade_01",                
+    "Grade 2": "grade_02",                
+    "Grade 3": "grade_03",                
+    "Grade 4": "grade_04",                
+    "Grade 5": "grade_05",                
+    "Grade 6": "grade_06",                           
+    "Grade 7": "grade_07",                
+    "Grade 8": "grade_08",                
+    "Grade 9": "grade_09",                
+    "Grade 10": "grade_10",                
+    "Grade 11": "grade_11",                
+    "Grade 12": "grade_12",                
+    "Staff Total": "staff_total",             
+    "Staff Teacher": "staff_teachers",          
+    "Staff Admin": "staff_admin",             
+    "Staff Pupil Services": "staff_pupil_svcs",        
+    "Staff Other": "staff_other"
+}
+
 
 with open('placeholder_name.csv', newline='', encoding='utf-8') as f:
     reader = csv.DictReader(f, fieldnames=None, restkey=None, restval=None, dialect='excel')
     for row in reader:
         print(row)
 
+
+try:
+    for row in csv.DictReader(f):
+        clean_schools_row = {schools_header_map[k]: v for k, v in row.items()}
+
+except:
+    print("error in using school_headers_map to create clean header row key for schools on ingest")
+
+    try:
+        for row in csv.DictReader(f):
+            clean_snapshots_row = {schools_snapshots_header_map[k]: v for k, v in row.items()}
+
+    except:
+        print("error in using schools_snapshots_headers_map to create clean header row key for schools snapshots table on ingest")
